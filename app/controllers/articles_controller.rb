5be31da536adcfbed0db0a4572114b5f4e20737b
Orko Elvis <ivanviter@i.ua>
2019-01-31 00:10:37 +0200

    Added some logic

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,25 @@ class ArticlesController < ApplicationController
     redirect_to @article
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to article_path
+  end
+
 private
   def article_params
     params.require(:article).permit(:title, :text)
