c40c50f50f62a8624ca85b73a17aa70cbae764da
orkoselvis <ivanviter@i.ua>
2019-02-08 17:44:20 +0200

    Added stylling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 module ApplicationHelper
-  
+
+  #if on active page color = white
+  #if not on active page color = regular
+
+  def active_link_to (nav_link, contrl, action, url, style)
+    if controller_name == contrl && action_name == action
+      return link_to nav_link, url, id: style
+    else
+      return link_to nav_link, url, class: style
+    end
+  end
+
 end
