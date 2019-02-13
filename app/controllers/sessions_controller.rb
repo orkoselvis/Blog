84dea8cbf563926b6ddd111f714611afd2d43b2b
orkoselvis <ivanviter@i.ua>
2019-02-13 18:05:13 +0200

    Added sign-in logic

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def index
+    redirect_to root_path
+  end
+
+  def new
+  end
+
+  def create
+    user = User.find_by_userid(params[:userid])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice:
+      "Welcome #{user.userid}, you successfully Signed In!"
+    else
+      render 'new', alert:
+      "User ID and/or Password Invalid!"
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates_uniqueness_of :email
+
+  
+
 end
