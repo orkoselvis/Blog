a169eca8eba8d94acae012a009fae8230432ddfc
orkoselvis <ivanviter@i.ua>
2019-02-13 16:50:58 +0200

    Added user logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_12_104305) do
+ActiveRecord::Schema.define(version: 2019_02_13_133604) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2019_02_12_104305) do
     t.string "l_name"
     t.string "email"
     t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "userid"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
