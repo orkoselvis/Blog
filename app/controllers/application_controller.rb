3ff3dedaf0541f16f3e9519574b1ab5fb2251456
Orko Elvis <ivanviter@i.ua>
2019-02-24 22:27:03 +0200

    Added link to about page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def show
+    render "pages/about"
+  end
+
   def require_user
     redirect_to root_path unless current_user
   end
