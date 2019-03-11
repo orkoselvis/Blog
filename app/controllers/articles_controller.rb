5b54d09339ee54470a38ad2c7b77104d17e36533
orkoselvis <ivanviter@i.ua>
2019-03-11 15:59:04 +0200

    Added some shit again

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ before_action :edit, :admin_authorize, :except => [:index, :show, :search]
 
   def new
     @article = Article.new
+    @categories = Category.all.map{|c| [c.name, c.id]}
   end
 
   def show
@@ -23,15 +24,22 @@ before_action :edit, :admin_authorize, :except => [:index, :show, :search]
 
   def create
     @article = Article.new(article_params)
-    if @article.save
-      redirect_to @article
-    else
-      render 'new'
+    @article.category_id = params[:category_id]
+
+    respond_to do |format|
+      if article.save
+        format.html { redirect_to @article, notice: "Article was successfully created!" }
+        format.json { render :show, status: :created, location: @article }
+      else
+        format.html { render :new}
+        format.json {render json: @article.errors, status: :unprocessable_entity}
+      end
     end
   end
 
   def edit
     @article = Article.find(params[:id])
+    @categories = Category.all.map { |c| [c.name, c.id]}
   end
 
   def search
@@ -44,6 +52,7 @@ before_action :edit, :admin_authorize, :except => [:index, :show, :search]
 
   def update
     @article = Article.find(params[:id])
+    @article.category_id = params[:category_id]
     if @article.update(article_params)
       redirect_to @article
     else
