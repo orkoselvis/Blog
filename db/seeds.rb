5f18266a2dd4f632a5b2ffa19f331dcae8b188ac
Orko Elvis <ivanviter@i.ua>
2019-05-21 15:19:36 +0300

    Added faker to subscribers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
 10.times do
 Article.create([{
   title: Faker::Science.element,
-  text: Faker::Science.scientist,
+  text: Faker::FunnyName.four_word_name,
   category_id: rand(1..4)
   }])
+end
+
+40.times do
+  Subscriber.create([{
+    f_name: Faker::Name.first_name,
+    l_name: Faker::Name.last_name,
+    country: Faker::Address.country,
+    email: Faker::Internet.email
+    }])
 end
