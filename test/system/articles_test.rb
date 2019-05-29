4a3bbbbc803d10f2723fcc9f2b67e822851799c4
Orko Elvis <ivanviter@i.ua>
2019-05-29 23:14:11 +0300

    Added some test data to fixtures and added some test cases

--- test/system/articles_test.rb
+++ test/system/articles_test.rb
@@ -11,15 +11,15 @@ class ArticlesTest < ApplicationSystemTestCase
     click_on "Sign In"
     fill_in "Input User ID", with: "orko"
     fill_in "Input Password", with: "1234567"
-    click_button"Sign In"
+    click_button "Sign In"
     click_on "Blog"
     click_on "Create New Article"
     fill_in "Title", with: "Some title :)"
     fill_in "Text", with: "And here is description :)"
-    click_on "Choose your category"
-    click_on "Movie"
-    click_on "Save Article"
-    assert_selector "p", text: "Article was successfully created!"
+    select "Choose your category"
+    select "Movie"
+    click_button "Save Article"
+    assert_selector 'h2', text: "TITLE:"
   end
 
 end
