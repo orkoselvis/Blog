e821684c288024424525b637ce0c2b6dc6f9d7ef
Orko Elvis <ivanviter@i.ua>
2019-05-31 23:06:01 +0300

    Added some test cases

--- spec/features/albums/index_spec.rb
+++ spec/features/albums/index_spec.rb
@@ -1,10 +1,25 @@
 require 'rails_helper'
 
 RSpec.feature 'Article list' do
-  scenario 'unauthenticated user' do
+  scenario 'Unauthenticated user' do
     visit articles_path
     within '#content' do
-      expect(find(h1)).to have_content('List of all Articles')
+      expect(page).to have_content('List of all Articles')
     end
+  end
+
+  scenario 'Read my Blog button' do
+    visit root_path
+    expect(page).to have_link("Read My Blog")
+  end
+
+  scenario 'First Name field' do
+    visit root_path
+    expect(page).to have_field("First Name")
+  end
+
+  scenario "Last Name field" do
+    visit root_path
+    expect(page).to have_field("Last Name")
   end
 end
