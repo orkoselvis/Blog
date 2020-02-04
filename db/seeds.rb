e53108e3675cf7d3fa800164578270005ab988f1
Orko Elvis <ivanviter@i.ua>
2020-02-04 19:50:44 +0200

    Modified seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-10.times do
-Article.create([{
-  title: Faker::Science.element,
-  text: Faker::Science.scientist,
-  category_id: rand(1..4)
+5.times do 
+  Category.create([{
+  name: ["Movies", "Sport", "Nature", "Action", "Hobbies"].sample
+  desc: "This is about it"
   }])
 end
+
+10.times do
+  Article.create([{
+  title: Faker::Science.element,
+  text: Faker::Science.scientist,
+  category_id: rand(1..5)
+  }])
+end
+
+
